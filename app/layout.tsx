68b9974e1a58b93932d85330f59856f9f9cac378
John Duru <kachijohnduru@googlemail.com>
2024-04-02 01:08:37 +0100

    changed project font

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,10 +1,12 @@
 import type { Metadata } from "next";
-import { Inter } from "next/font/google";
+import { Inter, Space_Mono } from "next/font/google";
 import "./globals.scss";
 import React from "react";
 import NavBar from "./components/Navbar/Navbar";
 
 const inter = Inter({ subsets: ["latin"] });
+
+const space_mono = Space_Mono({ subsets: ["latin"], weight: ['400', '700']})
 
 export const metadata: Metadata = {
   title: "Create Next App",
@@ -18,7 +20,7 @@ export default function RootLayout({
 }>) {
   return (
     <html lang="en">
-      <body className={inter.className}>
+      <body className={space_mono.className}>
         <NavBar/>
         {children}</body>
     </html>
